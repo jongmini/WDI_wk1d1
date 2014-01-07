fce376b363ecee5462e7b0c06f8e30ed9ec9777d
jongmini <jongminiz@yahoo.com>
2014-01-07 09:53:00 -0800

    first commit

---
+++ guess.rb
@@ -0,0 +1,19 @@
+#jongmini/code/guess.rb
+
+puts "Guess a number between 1 and 100"
+num = gets.chomp.to_i
+random = rand(1..100)
+i = 1
+  	while num != random 
+  		if num > random
+  		puts "The number is lower than #{num}. Guess again."
+  		i += 1
+  		num = gets.chomp.to_i
+  		elsif num < random
+  		puts "The number is higher than #{num}. Guess again."
+  		i += 1
+  		num = gets.chomp.to_i
+  		end
+   	end
+puts "You got it in #{i} tries."
+
